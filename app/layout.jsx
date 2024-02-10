50e677916b45557a6352dd77eb1e676138ce9536
Yassine <essebbar.yassine@gmail.com>
2024-02-11 00:35:59 +0100

    Add SpeedInsights component to layout.jsx and update package dependencies

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Components
 import Header from "@/components/Header";
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
           <Header />
           {children}
           <Footer />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
