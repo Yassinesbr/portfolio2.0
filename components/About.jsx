18ad9620c096f45828d912bf4f17c0c3ee404028
Yassine sebbar <essebbar.yassine@gmail.com>
2024-02-16 17:49:32 +0100

    Update About component and tailwind.config.js

--- components/About.jsx
+++ components/About.jsx
@@ -12,6 +12,7 @@ import {
   Briefcase,
 } from "lucide-react";
 import { yearsOfExperience } from "./Hero";
+import { get } from "react-hook-form";
 
 const infoData = [
   {
@@ -51,7 +52,7 @@ const qualificationsData = [
       },
       {
         university: "Faculty of Sciences, Rabat",
-        qualification: "Bachelor's degree in Computer Science",
+        qualification: "Licentiate in Computer Science",
         years: "2015 - 2019",
       },
       {
@@ -112,7 +113,7 @@ const skillData = [
         imgPath: "/about/vscode.svg",
       },
       {
-        imgPath: "/about/react.svg",
+        imgPath: "/about/figma.svg",
       },
       {
         imgPath: "/about/notion.svg",
@@ -192,10 +193,10 @@ const About = () => {
                     <h3 className="h3 mb-3 text-center xl:text-left">
                       Journey
                     </h3>
-                    <div>
-                      <div>
+                    <div className="grid md:grid-cols-2 gap-y-8">
+                      <div className="flex flex-col gap-y-6">
                         <div className="flex gap-x-4 items-center text-[22px] text-primary">
-                          <Briefcase />
+                          <Briefcase size={28} />
                           <h4 className="capitalize font-medium">
                             {getData(qualificationsData, "experience").title}
                           </h4>
@@ -206,6 +207,9 @@ const About = () => {
                               const { company, qualification, years } = item;
                               return (
                                 <div key={index} className="flex gap-x-8 group">
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-1000"></div>
+                                  </div>
                                   <div>
                                     <div className="font-semibold text-xl leading-none mb-2">
                                       {company}
@@ -223,11 +227,87 @@ const About = () => {
                           )}
                         </div>
                       </div>
-                      <div>education</div>
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
+                          <GraduationCap size={28} />
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationsData, "education").title}
+                          </h4>
+                        </div>
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationsData, "education").data.map(
+                            (item, index) => {
+                              const { university, qualification, years } = item;
+                              return (
+                                <div key={index} className="flex gap-x-8 group">
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-1000"></div>
+                                  </div>
+                                  <div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
+                                  </div>
+                                </div>
+                              );
+                            }
+                          )}
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </TabsContent>
-                <TabsContent value="skills"> Skills Infos</TabsContent>
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Tools I Use</h3>
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
+                      <div>
+                        {getData(skillData, "skills").data.map(
+                          (item, index) => {
+                            const { name } = item;
+                            return (
+                              <div
+                                className="w-2/4 text-center xl:text-left mx-auto xl:mx-0"
+                                key={index}
+                              >
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            );
+                          }
+                        )}
+                      </div>
+                    </div>
+                    <div>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
+                        Tools
+                      </h4>
+                      <div className="border-b border-border mb-4"></div>
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, "tools").data.map((item, index) => {
+                          const { imgPath } = item;
+                          return (
+                            <div key={index}>
+                              <Image
+                                src={imgPath}
+                                alt="tool"
+                                width={48}
+                                height={48}
+                              />
+                            </div>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </TabsContent>
               </div>
             </Tabs>
           </div>
